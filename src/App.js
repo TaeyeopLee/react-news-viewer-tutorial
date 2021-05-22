7e74823e8997729850cbe6c6ec825a2d9bb1b5f2
TaeyeopLee <lee.taeyeop@hotmail.com>
2021-05-22 16:03:47 +0900

    add categories

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React, { useState } from 'react';
 import NewsList from './components/NewsList';
-import axios from 'axios';
+import Categories from './components/Categories';
 
 const App = () => {
-  // const [data, setData] = useState(null);
-  // const onClick = async () => {
-  //   try {
-  //     const apikey = '4770a2be4ab342078f5e967167706d88'
-  //     const newsurl = 'https://newsapi.org/v2/top-headlines?country=kr&apiKey='
-  //     const response = await axios.get(newsurl+apikey);
-  //     setData(response.data);
-  //   }
-  //   catch (e) {
-  //     console.error(e);
-  //   }
-  // }
-
-  // return (
-  //   <div>
-  //     <div>
-  //       <button onClick={onClick}>불러오기</button>
-  //     </div>
-  //     {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-  //   </div>
-  // );
-  return <NewsList />;
+  return (
+    <>
+      <Categories></Categories>
+      <NewsList></NewsList>
+    </>
+  );
 };
 
 export default App;
