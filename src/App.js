c452bdc0194e19118995111ddf12dbc38e46b609
TaeyeopLee <lee.taeyeop@hotmail.com>
2021-05-22 15:38:09 +0900

    news-viewer api request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NewsList from './components/NewsList';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const apikey = '4770a2be4ab342078f5e967167706d88'
+  //     const newsurl = 'https://newsapi.org/v2/top-headlines?country=kr&apiKey='
+  //     const response = await axios.get(newsurl+apikey);
+  //     setData(response.data);
+  //   }
+  //   catch (e) {
+  //     console.error(e);
+  //   }
+  // }
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <button onClick={onClick}>불러오기</button>
+  //     </div>
+  //     {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+  //   </div>
+  // );
+  return <NewsList />;
+};
 
 export default App;
